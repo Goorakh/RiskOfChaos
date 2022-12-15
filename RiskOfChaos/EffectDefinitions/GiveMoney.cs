56af4a308647b1103f56fbcd2f471045b2b7b014
a22petoh <petter.ohlen@gmail.com>
2022-12-15 18:13:06 +0100

    Add effect "Payday"

---
+++ RiskOfChaos/EffectDefinitions/GiveMoney.cs
@@ -0,0 +1,22 @@
+﻿using RiskOfChaos.EffectHandling;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskOfChaos.EffectDefinitions
+{
+    [ChaosEffect("GiveMoney")]
+    public class GiveMoney : BaseEffect
+    {
+        public override void OnStart()
+        {
+            uint amount = (uint)Run.instance.GetDifficultyScaledCost(200);
+
+            foreach (CharacterMaster master in PlayerUtils.GetAllPlayerMasters(false))
+            {
+                master.GiveMoney(amount);
+            }
+        }
+    }
+}
