9328c30373a9340247f98ed03aea457de823cb88
a22petoh <petter.ohlen@gmail.com>
2022-12-17 04:21:02 +0100

    Move ChaosEffectActivationCounter to own file

--- RiskOfChaos/EffectHandling/ChaosEffectDispatcher.cs
+++ RiskOfChaos/EffectHandling/ChaosEffectDispatcher.cs
@@ -9,43 +9,6 @@ using UnityEngine.Networking;
 
 namespace RiskOfChaos.EffectHandling
 {
-    public struct ChaosEffectActivationCounter
-    {
-        public static readonly ChaosEffectActivationCounter EmptyCounter = new ChaosEffectActivationCounter(-1);
-
-        public readonly int EffectIndex;
-
-        public int TotalActivations { get; private set; }
-
-        public int StageActivations { get; private set; }
-
-        public ChaosEffectActivationCounter(int effectIndex)
-        {
-            EffectIndex = effectIndex;
-
-            TotalActivations = 0;
-            StageActivations = 0;
-
-            if (effectIndex != -1)
-            {
-                Stage.onServerStageComplete += Stage_onServerStageComplete;
-                Run.onRunDestroyGlobal += Run_onRunDestroyGlobal;
-            }
-        }
-
-        void Stage_onServerStageComplete(Stage stage)
-        {
-            TotalActivations += StageActivations;
-            StageActivations = 0;
-        }
-
-        void Run_onRunDestroyGlobal(Run run)
-        {
-            TotalActivations = 0;
-            StageActivations = 0;
-        }
-    }
-
     public static class ChaosEffectDispatcher
     {
         static float _nextEffectDispatchTime = float.PositiveInfinity;
