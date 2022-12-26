4037993ba12ec1fbaca1cdded22d037683ef39bd
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2022-12-26 18:25:43 +0100

    Added effect "Corrupt Random Item"

---
+++ RiskOfChaos/EffectDefinitions/Items/CorruptRandomItem.cs
@@ -0,0 +1,58 @@
+﻿using RiskOfChaos.EffectHandling;
+using RiskOfChaos.Utilities;
+using RoR2;
+using RoR2.EntitlementManagement;
+using RoR2.ExpansionManagement;
+using RoR2.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiskOfChaos.EffectDefinitions.Items
+{
+    [ChaosEffect("CorruptRandomItem", DefaultSelectionWeight = 0.6f, EffectRepetitionWeightCalculationMode = EffectActivationCountMode.PerRun)]
+    public class CorruptRandomItem : BaseEffect
+    {
+        [EffectCanActivate]
+        static bool CanActivate()
+        {
+            return ExpansionUtils.IsExpansionEnabled(ExpansionUtils.DLC1_NAME) && PlayerUtils.GetAllPlayerMasters(false).Any(m => getAllCorruptableItems(m.inventory).Any());
+        }
+
+        static IEnumerable<ItemIndex> getAllCorruptableItems(Inventory inventory)
+        {
+            Run run = Run.instance;
+            if (!run || !inventory)
+                yield break;
+
+            foreach (ItemIndex item in ItemCatalog.allItems)
+            {
+                if (inventory.GetItemCount(item) <= 0)
+                    continue;
+
+                ItemIndex transformedItem = ContagiousItemManager.GetTransformedItemIndex(item);
+                if (transformedItem == ItemIndex.None || !run.IsItemAvailable(transformedItem))
+                    continue;
+                
+                yield return item;
+            }
+        }
+
+        public override void OnStart()
+        {
+            foreach (CharacterMaster playerMaster in PlayerUtils.GetAllPlayerMasters(false))
+            {
+                Inventory inventory = playerMaster.inventory;
+                if (!inventory)
+                    continue;
+
+                ItemIndex[] allCorruptableItems = getAllCorruptableItems(inventory).ToArray();
+                if (allCorruptableItems.Length <= 0)
+                    continue;
+
+                ContagiousItemManager.TryForceReplacement(inventory, RNG.NextElementUniform(allCorruptableItems));
+            }
+        }
+    }
+}
---
+++ RiskOfChaos/Utilities/ExpansionUtils.cs
@@ -0,0 +1,30 @@
+﻿using RoR2;
+using RoR2.ExpansionManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskOfChaos.Utilities
+{
+    public static class ExpansionUtils
+    {
+        public const string DLC1_NAME = "DLC1";
+
+        public static ExpansionDef FindExpansionDef(string name)
+        {
+            foreach (ExpansionDef expansion in ExpansionCatalog.expansionDefs)
+            {
+                if (expansion.name == name)
+                    return expansion;
+            }
+
+            return null;
+        }
+
+        public static bool IsExpansionEnabled(string name)
+        {
+            ExpansionDef expansionDef = FindExpansionDef(name);
+            return expansionDef && Run.instance && Run.instance.IsExpansionEnabled(expansionDef);
+        }
+    }
+}
