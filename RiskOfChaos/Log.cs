dbf74728d77bee83fbb1969d22bc10e23ae3114b
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2022-12-27 14:53:18 +0100

    Remove unnecessary start of path in console logs

--- RiskOfChaos/Log.cs
+++ RiskOfChaos/Log.cs
@@ -12,13 +12,26 @@ namespace RiskOfChaos
             _logSource = logSource;
         }
 
+        static string getLogPrefix(string callerPath, int callerLineNumber)
+        {
+            const string MOD_NAME = "RiskOfChaos";
+
+            int modNameLastPathIndex = callerPath.LastIndexOf(MOD_NAME);
+            if (modNameLastPathIndex >= 0)
+            {
+                callerPath = callerPath.Substring(modNameLastPathIndex + MOD_NAME.Length + 1);
+            }
+
+            return $"{callerPath}:{callerLineNumber} ";
+        }
+
 #if DEBUG
-        internal static void Debug(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogDebug(includeCallerInfo ? $"{callerPath}:{callerLineNumber} {data}" : data);
+        internal static void Debug(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogDebug(includeCallerInfo ? getLogPrefix(callerPath, callerLineNumber) + data : data);
 #endif
-        internal static void Error(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogError(includeCallerInfo ? $"{callerPath}:{callerLineNumber} {data}" : data);
-        internal static void Fatal(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogFatal(includeCallerInfo ? $"{callerPath}:{callerLineNumber} {data}" : data);
-        internal static void Info(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogInfo(includeCallerInfo ? $"{callerPath}:{callerLineNumber} {data}" : data);
-        internal static void Message(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogMessage(includeCallerInfo ? $"{callerPath}:{callerLineNumber} {data}" : data);
-        internal static void Warning(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogWarning(includeCallerInfo ? $"{callerPath}:{callerLineNumber} {data}" : data);
+        internal static void Error(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogError(includeCallerInfo ? getLogPrefix(callerPath, callerLineNumber) + data : data);
+        internal static void Fatal(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogFatal(includeCallerInfo ? getLogPrefix(callerPath, callerLineNumber) + data : data);
+        internal static void Info(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogInfo(includeCallerInfo ? getLogPrefix(callerPath, callerLineNumber) + data : data);
+        internal static void Message(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogMessage(includeCallerInfo ? getLogPrefix(callerPath, callerLineNumber) + data : data);
+        internal static void Warning(object data, bool includeCallerInfo = true, [CallerFilePath] string callerPath = default, [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogWarning(includeCallerInfo ? getLogPrefix(callerPath, callerLineNumber) + data : data);
     }
 }
