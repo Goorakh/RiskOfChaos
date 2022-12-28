66f8e4a463cb066121d3858aec2f869e55cd6c38
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2022-12-28 17:17:53 +0100

    OrderInventory: Don't activate if no players will be affected by it

--- RiskOfChaos/EffectDefinitions/Items/OrderInventory.cs
+++ RiskOfChaos/EffectDefinitions/Items/OrderInventory.cs
@@ -1,6 +1,7 @@
 ﻿using RiskOfChaos.EffectHandling;
 using RiskOfChaos.Utilities;
 using RoR2;
+using System.Linq;
 using UnityEngine;
 
 namespace RiskOfChaos.EffectDefinitions.Items
@@ -8,6 +9,32 @@ namespace RiskOfChaos.EffectDefinitions.Items
     [ChaosEffect("OrderInventory", DefaultSelectionWeight = 0.2f, EffectRepetitionWeightExponent = 100f, EffectRepetitionWeightCalculationMode = EffectActivationCountMode.PerRun)]
     public class OrderInventory : BaseEffect
     {
+        [EffectCanActivate]
+        static bool CanActivate()
+        {
+            return PlayerUtils.GetAllPlayerMasters(false).Any(m => ItemTierCatalog.allItemTierDefs.Any(itd => itd.canRestack && hasAtLeastXUniqueItemsInTier(m.inventory, itd.tier, 2)));
+        }
+
+        static bool hasAtLeastXUniqueItemsInTier(Inventory inventory, ItemTier itemTier, int count)
+        {
+            if (!inventory)
+                return false;
+
+            int totalCount = 0;
+            foreach (ItemDef item in ItemCatalog.allItemDefs)
+            {
+                if (inventory.GetItemCount(item) > 0 && item.tier == itemTier)
+                {
+                    if (++totalCount >= count)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
         public override void OnStart()
         {
             foreach (CharacterBody playerBody in PlayerUtils.GetAllPlayerBodies(false))
