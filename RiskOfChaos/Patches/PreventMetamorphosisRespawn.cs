3be23b41fd67e9be0d536bd6e4f5e7e832bb12c8
a22petoh <petter.ohlen@gmail.com>
2023-01-04 00:00:57 +0100

    PreventMetamorphosisRespawn: Only apply hook when needed

--- RiskOfChaos/Patches/PreventMetamorphosisRespawn.cs
+++ RiskOfChaos/Patches/PreventMetamorphosisRespawn.cs
@@ -4,35 +4,57 @@ using MonoMod.Cil;
 using RoR2;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace RiskOfChaos.Patches
 {
     static class PreventMetamorphosisRespawn
     {
-        internal static bool PreventionEnabled;
+        static bool _hasAppliedPatch;
 
-        [SystemInitializer]
-        static void Init()
+        static readonly FieldInfo _preventionEnabled_FI = AccessTools.DeclaredField(typeof(PreventMetamorphosisRespawn), nameof(_preventionEnabled));
+        static bool _preventionEnabled;
+
+        internal static bool PreventionEnabled
         {
-            IL.RoR2.CharacterMaster.Respawn += static il =>
+            get
             {
-                ILCursor c = new ILCursor(il);
+                return _preventionEnabled;
+            }
+            set
+            {
+                _preventionEnabled = value;
 
-                ILCursor[] foundCursors;
-                if (c.TryFindNext(out foundCursors,
-                                  x => x.MatchCallOrCallvirt(AccessTools.DeclaredPropertyGetter(typeof(RoR2Content.Artifacts), nameof(RoR2Content.Artifacts.randomSurvivorOnRespawnArtifactDef))),
-                                  x => x.MatchCallOrCallvirt(SymbolExtensions.GetMethodInfo<RunArtifactManager>(_ => _.IsArtifactEnabled(default(ArtifactDef)))),
-                                  x => x.MatchBrfalse(out _)))
+                if (!_hasAppliedPatch && _preventionEnabled)
                 {
-                    ILCursor cursor = foundCursors[2];
-                    cursor.Next.MatchBrfalse(out ILLabel afterIfLbl);
-                    cursor.Index++;
-
-                    cursor.EmitDelegate(static () => PreventionEnabled);
-                    cursor.Emit(OpCodes.Brtrue, afterIfLbl);
+                    IL.RoR2.CharacterMaster.Respawn += CharacterMaster_Respawn;
+                    _hasAppliedPatch = true;
                 }
-            };
+            }
+        }
+
+        static void CharacterMaster_Respawn(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            ILCursor[] foundCursors;
+            if (c.TryFindNext(out foundCursors,
+                              x => x.MatchCallOrCallvirt(AccessTools.DeclaredPropertyGetter(typeof(RoR2Content.Artifacts), nameof(RoR2Content.Artifacts.randomSurvivorOnRespawnArtifactDef))),
+                              x => x.MatchCallOrCallvirt(SymbolExtensions.GetMethodInfo<RunArtifactManager>(_ => _.IsArtifactEnabled(default(ArtifactDef)))),
+                              x => x.MatchBrfalse(out _)))
+            {
+                ILCursor cursor = foundCursors[2];
+                cursor.Next.MatchBrfalse(out ILLabel afterIfLbl);
+                cursor.Index++;
+
+                cursor.Emit(OpCodes.Ldsfld, _preventionEnabled_FI);
+                cursor.Emit(OpCodes.Brtrue, afterIfLbl);
+            }
+            else
+            {
+                Log.Error("Unable to find patch location");
+            }
         }
     }
 }
