a47364f1c6e3ce68aa23071ee8f468236cda5e3c
a22petoh <petter.ohlen@gmail.com>
2023-03-14 20:33:59 +0100

    Add effect: Give Tonic Affliction

---
+++ RiskOfChaos/EffectDefinitions/Character/Player/Items/GiveTonicAffliction.cs
@@ -0,0 +1,18 @@
+﻿using RiskOfChaos.EffectHandling;
+using RiskOfChaos.Utilities;
+using RoR2;
+
+namespace RiskOfChaos.EffectDefinitions.Character.Player.Items
+{
+    [ChaosEffect("give_tonic_affliction", DefaultSelectionWeight = 0.4f, EffectRepetitionWeightCalculationMode = EffectActivationCountMode.PerRun, EffectWeightReductionPercentagePerActivation = 40f)]
+    public class GiveTonicAffliction : BaseEffect
+    {
+        public override void OnStart()
+        {
+            foreach (CharacterMaster master in PlayerUtils.GetAllPlayerMasters(false))
+            {
+                master.inventory.GiveItem(RoR2Content.Items.TonicAffliction);
+            }
+        }
+    }
+}
