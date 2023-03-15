0a0eeb0897c210a08102c7ea9ce37695ecde217e
a22petoh <petter.ohlen@gmail.com>
2023-03-15 20:44:30 +0100

    Add effect: Max All Cooldowns

---
+++ RiskOfChaos/EffectDefinitions/Character/Player/MaxCooldowns.cs
@@ -0,0 +1,44 @@
+﻿using RiskOfChaos.EffectHandling;
+using RiskOfChaos.Utilities;
+using RoR2;
+
+namespace RiskOfChaos.EffectDefinitions.Character.Player
+{
+    [ChaosEffect("max_cooldowns", DefaultSelectionWeight = 0.6f, EffectRepetitionWeightCalculationMode = EffectActivationCountMode.PerRun, EffectWeightReductionPercentagePerActivation = 20f)]
+    public class MaxCooldowns : BaseEffect
+    {
+        public override void OnStart()
+        {
+            foreach (CharacterBody body in PlayerUtils.GetAllPlayerBodies(true))
+            {
+                int skillSlotCount = body.skillLocator.skillSlotCount;
+                for (int i = 0; i < skillSlotCount; i++)
+                {
+                    GenericSkill skill = body.skillLocator.GetSkillAtIndex(i);
+                    if (skill)
+                    {
+                        skill.RemoveAllStocks();
+                    }
+                }
+
+                Inventory inventory = body.inventory;
+                if (inventory)
+                {
+                    int equipmentSlotCount = inventory.GetEquipmentSlotCount();
+                    for (uint i = 0; i < equipmentSlotCount; i++)
+                    {
+                        EquipmentState equipmentState = inventory.GetEquipment(i);
+                        if (equipmentState.equipmentIndex != EquipmentIndex.None)
+                        {
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public
+                            float equipmentCooldown = equipmentState.equipmentDef.cooldown * inventory.CalculateEquipmentCooldownScale();
+#pragma warning restore Publicizer001 // Accessing a member that was not originally public
+
+                            inventory.SetEquipment(new EquipmentState(equipmentState.equipmentIndex, Run.FixedTimeStamp.now + equipmentCooldown, 0), i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
