7adc9682b9a1c83215f2948f40bfc02477912bfd
a22petoh <petter.ohlen@gmail.com>
2023-03-16 17:08:47 +0100

    Add effect: Poverty

---
+++ RiskOfChaos/EffectDefinitions/Character/Player/RemoveAllMoney.cs
@@ -0,0 +1,18 @@
+﻿using RiskOfChaos.EffectHandling;
+using RiskOfChaos.Utilities;
+using RoR2;
+
+namespace RiskOfChaos.EffectDefinitions.Character.Player
+{
+    [ChaosEffect("remove_all_money", DefaultSelectionWeight = 0.6f, EffectWeightReductionPercentagePerActivation = 30f)]
+    public sealed class RemoveAllMoney : BaseEffect
+    {
+        public override void OnStart()
+        {
+            foreach (CharacterMaster master in PlayerUtils.GetAllPlayerMasters(false))
+            {
+                master.money = 0;
+            }
+        }
+    }
+}
