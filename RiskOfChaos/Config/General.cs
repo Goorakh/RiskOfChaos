ec34a6390702814d557eeb04fa1ae438edc6c0c3
a22petoh <petter.ohlen@gmail.com>
2023-03-20 14:41:35 +0100

    Add localhost connect setting

--- RiskOfChaos/Config/General.cs
+++ RiskOfChaos/Config/General.cs
@@ -39,6 +39,10 @@ namespace RiskOfChaos.Config
             public static bool DebugDisable => _debugDisable?.Value ?? DEBUG_DISABLE_DEFAULT_VALUE;
 
             public static event Action OnDebugDisabledChanged;
+
+            static ConfigEntry<bool> _useLocalhostConnect;
+            const bool USE_LOCALHOST_CONNECT_DEFAULT_VALUE = false;
+            public static bool UseLocalhostConnect => _useLocalhostConnect?.Value ?? USE_LOCALHOST_CONNECT_DEFAULT_VALUE;
 #endif
 
             internal static void Init(ConfigFile file)
@@ -63,12 +67,31 @@ namespace RiskOfChaos.Config
                 };
 
 #if DEBUG
-                _debugDisable = file.Bind(new ConfigDefinition(SECTION_NAME, "Debug Disable"), DEBUG_DISABLE_DEFAULT_VALUE);
+                const string DEBUG_SECTION_NAME = "Debug";
+
+                _debugDisable = file.Bind(new ConfigDefinition(DEBUG_SECTION_NAME, "Debug Disable"), DEBUG_DISABLE_DEFAULT_VALUE);
                 ModSettingsManager.AddOption(new CheckBoxOption(_debugDisable), GUID, NAME);
 
                 _debugDisable.SettingChanged += static (s, e) =>
                 {
                     OnDebugDisabledChanged?.Invoke();
+                };
+
+                _useLocalhostConnect = file.Bind(new ConfigDefinition(DEBUG_SECTION_NAME, "Use Localhost Connect"), USE_LOCALHOST_CONNECT_DEFAULT_VALUE);
+                ModSettingsManager.AddOption(new CheckBoxOption(_useLocalhostConnect), GUID, NAME);
+
+                On.RoR2.Networking.NetworkManagerSystem.ClientSendAuth += (orig, self, conn) =>
+                {
+                    if (UseLocalhostConnect)
+                        return;
+
+                    orig(self, conn);
+                };
+
+                // Connecting to localhost this way makes entitlements not work, so just force them all to be enabled
+                On.RoR2.PlayerCharacterMasterControllerEntitlementTracker.HasEntitlement += (orig, self, entitlementDef) =>
+                {
+                    return UseLocalhostConnect || orig(self, entitlementDef);
                 };
 #endif
 
