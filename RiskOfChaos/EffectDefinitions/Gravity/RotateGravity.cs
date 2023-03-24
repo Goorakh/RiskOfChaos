27c33c7d702e6016c84d773e68152b7677449e92
a22petoh <petter.ohlen@gmail.com>
2023-03-24 19:52:51 +0100

    Random Gravity Direction: Add default weights and config

--- RiskOfChaos/EffectDefinitions/Gravity/RotateGravity.cs
+++ RiskOfChaos/EffectDefinitions/Gravity/RotateGravity.cs
@@ -1,13 +1,18 @@
-﻿using HarmonyLib;
+﻿using BepInEx.Configuration;
+using HarmonyLib;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using RiskOfChaos.EffectHandling;
 using RiskOfChaos.EffectHandling.EffectClassAttributes;
+using RiskOfChaos.EffectHandling.EffectClassAttributes.Data;
+using RiskOfOptions.OptionConfigs;
+using RiskOfOptions.Options;
 using RoR2;
 using UnityEngine;
 
 namespace RiskOfChaos.EffectDefinitions.Gravity
 {
-    [ChaosEffect("rotate_gravity")]
+    [ChaosEffect("rotate_gravity", DefaultSelectionWeight = 0.8f, EffectWeightReductionPercentagePerActivation = 20f)]
     public sealed class RotateGravity : GenericGravityEffect
     {
         static bool _hasAppliedPatches = false;
@@ -67,18 +72,60 @@ namespace RiskOfChaos.EffectDefinitions.Gravity
             _hasAppliedPatches = true;
         }
 
+        [InitEffectInfo]
+        static readonly ChaosEffectInfo _effectInfo;
+
+        static ConfigEntry<float> _maxDeviationConfig;
+        const float MAX_DEVITATION_DEFAULT_VALUE = 30f;
+
+        const float MAX_DEVITATION_MIN_VALUE = 0f;
+        const float MAX_DEVITATION_MAX_VALUE = 90f;
+
+        static float maxDeviation
+        {
+            get
+            {
+                if (_maxDeviationConfig == null)
+                {
+                    return MAX_DEVITATION_DEFAULT_VALUE;
+                }
+                else
+                {
+                    return Mathf.Clamp(MAX_DEVITATION_MIN_VALUE, MAX_DEVITATION_MAX_VALUE, _maxDeviationConfig.Value);
+                }
+            }
+        }
+
+        [SystemInitializer(typeof(ChaosEffectCatalog))]
+        static void InitConfig()
+        {
+            _maxDeviationConfig = Main.Instance.Config.Bind(new ConfigDefinition(_effectInfo.ConfigSectionName, "Max Rotation Angle"), MAX_DEVITATION_DEFAULT_VALUE, new ConfigDescription("The maximum amount of deviation (in degrees) that can be applied to the gravity direction"));
+
+            addConfigOption(new StepSliderOption(_maxDeviationConfig, new StepSliderConfig
+            {
+                formatString = "{0:F1}",
+                min = MAX_DEVITATION_MIN_VALUE,
+                max = MAX_DEVITATION_MAX_VALUE,
+                increment = 0.5f
+            }));
+        }
+
+        Quaternion _gravityRotation;
+
         protected override Vector3 modifyGravity(Vector3 originalGravity)
         {
-            const float MAX_DEVIATION = 30f;
-
-            return Quaternion.Euler(RNG.RangeFloat(-MAX_DEVIATION, MAX_DEVIATION),
-                                    RNG.RangeFloat(-MAX_DEVIATION, MAX_DEVIATION),
-                                    RNG.RangeFloat(-MAX_DEVIATION, MAX_DEVIATION)) * originalGravity;
+            return _gravityRotation * originalGravity;
         }
 
         public override void OnStart()
         {
             tryApplyPatches();
+
+            float maxDeviation = RotateGravity.maxDeviation;
+            _gravityRotation = Quaternion.Euler(RNG.RangeFloat(-maxDeviation, maxDeviation),
+                                                RNG.RangeFloat(-maxDeviation, maxDeviation),
+                                                RNG.RangeFloat(-maxDeviation, maxDeviation));
+
             base.OnStart();
         }
     }
