f9b33c32e24cc328fa1fc4421ff9a77eda070f4c
a22petoh <petter.ohlen@gmail.com>
2023-03-24 15:40:43 +0100

    Added effect: Random Gravity Direction

--- RiskOfChaos/EffectDefinitions/Gravity/GenericMultiplyGravityEffect.cs
+++ RiskOfChaos/EffectDefinitions/Gravity/GenericMultiplyGravityEffect.cs
@@ -1,98 +1,14 @@
-﻿using RiskOfChaos.EffectHandling.EffectClassAttributes.Methods;
-using RiskOfChaos.Networking;
-using RoR2;
-using System;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RiskOfChaos.EffectDefinitions.Gravity
 {
-    public abstract class GenericMultiplyGravityEffect : BaseEffect
+    public abstract class GenericMultiplyGravityEffect : GenericGravityEffect
     {
         protected abstract float multiplier { get; }
 
-        static bool _hasGravityChangedThisStage = false;
-
-        static bool _hasRestoreEvent = false;
-        static void tryAddRestoreEventListener()
+        protected override sealed Vector3 modifyGravity(Vector3 originalGravity)
         {
-            if (_hasRestoreEvent)
-                return;
-
-#if DEBUG
-            Log.Debug("adding reset gravity event listener");
-#endif
-
-            Stage.onServerStageComplete += static _ =>
-            {
-                tryResetGravity();
-            };
-
-            // onServerStageComplete doesn't happen if the run is lost or exited prematurely, so make sure to reset the gravity regardless of how the stage ends
-            Run.onRunDestroyGlobal += static _ =>
-            {
-                tryResetGravity();
-            };
-
-            _hasRestoreEvent = true;
-        }
-
-        static void tryResetGravity()
-        {
-            if (_hasGravityChangedThisStage)
-            {
-#if DEBUG
-                Log.Debug("resetting gravity");
-#endif
-
-                // Don't network this setter, since it's either called at run destroy, or end of start (in which case it might get received too late and override the stage gravity), _hasGravityChangedThisStage is networked anyway
-                Physics.gravity = new Vector3(0f, Run.baseGravity, 0f);
-
-                _hasGravityChangedThisStage = false;
-            }
-        }
-
-        static void onGravityChanged()
-        {
-            _hasGravityChangedThisStage = true;
-            tryAddRestoreEventListener();
-        }
-
-        public override void OnStart()
-        {
-            SyncSetGravity.NetworkedGravity *= multiplier;
-            onGravityChanged();
-        }
-
-        [SystemInitializer]
-        static void InitNetworkEventListener()
-        {
-            SyncSetGravity.OnReceive += SyncSetGravity_OnReceive;
-        }
-
-        static void SyncSetGravity_OnReceive(in Vector3 newGravity)
-        {
-            onGravityChanged();
-        }
-
-        static SceneIndex[] _invalidOnScenes;
-
-        [SystemInitializer(typeof(SceneCatalog))]
-        static void InitScenes()
-        {
-            _invalidOnScenes = new SceneIndex[]
-            {
-                // Temporary fix: Changed gravity will potentially softlock due to the jump pads not going the correct height. Could also be solved by changing the applied force, but this works for now
-                SceneCatalog.FindSceneIndex("moon"),
-                SceneCatalog.FindSceneIndex("moon2")
-            }.Where(static i => i != SceneIndex.Invalid).OrderBy(static i => i).ToArray();
-        }
-
-        [EffectCanActivate]
-        static bool CanActivate()
-        {
-            SceneDef currentScene = SceneCatalog.GetSceneDefForCurrentScene();
-            return currentScene && Array.BinarySearch(_invalidOnScenes, currentScene.sceneDefIndex) < 0;
+            return originalGravity * multiplier;
         }
     }
 }
---
+++ RiskOfChaos/EffectDefinitions/Gravity/RotateGravity.cs
@@ -0,0 +1,85 @@
+﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RiskOfChaos.EffectHandling.EffectClassAttributes;
+using RoR2;
+using UnityEngine;
+
+namespace RiskOfChaos.EffectDefinitions.Gravity
+{
+    [ChaosEffect("rotate_gravity")]
+    public sealed class RotateGravity : GenericGravityEffect
+    {
+        static bool _hasAppliedPatches = false;
+
+        static void tryApplyPatches()
+        {
+            if (_hasAppliedPatches)
+                return;
+
+            IL.RoR2.CharacterMotor.PreMove += il =>
+            {
+                ILCursor c = new ILCursor(il);
+
+                ILCursor[] foundCursors;
+                if (c.TryFindNext(out foundCursors,
+                                  x => x.MatchLdarg(0),
+                                  x => x.MatchCall(AccessTools.DeclaredPropertyGetter(typeof(CharacterMotor), nameof(CharacterMotor.useGravity))),
+                                  x => x.MatchBrfalse(out _)))
+                {
+                    ILCursor cursor = foundCursors[2];
+                    cursor.Index++;
+
+                    cursor.Emit(OpCodes.Ldarg_0);
+                    cursor.Emit(OpCodes.Ldarg_1);
+                    cursor.EmitDelegate((CharacterMotor instance, float deltaTime) =>
+                    {
+                        if (!AnyGravityChangeActive)
+                            return;
+
+                        Vector3 xzGravity = new Vector3(Physics.gravity.x, 0f, Physics.gravity.z);
+                        instance.velocity += xzGravity * deltaTime;
+                    });
+                }
+            };
+
+            IL.RoR2.ModelLocator.UpdateTargetNormal += il =>
+            {
+                ILCursor c = new ILCursor(il);
+
+                while (c.TryGotoNext(MoveType.After,
+                                     x => x.MatchCallOrCallvirt(AccessTools.DeclaredPropertyGetter(typeof(Vector3), nameof(Vector3.up)))))
+                {
+                    c.EmitDelegate((Vector3 up) =>
+                    {
+                        if (AnyGravityChangeActive)
+                        {
+                            return -Physics.gravity.normalized;
+                        }
+                        else
+                        {
+                            return up;
+                        }
+                    });
+                }
+            };
+
+            _hasAppliedPatches = true;
+        }
+
+        protected override Vector3 modifyGravity(Vector3 originalGravity)
+        {
+            const float MAX_DEVIATION = 30f;
+
+            return Quaternion.Euler(RNG.RangeFloat(-MAX_DEVIATION, MAX_DEVIATION),
+                                    RNG.RangeFloat(-MAX_DEVIATION, MAX_DEVIATION),
+                                    RNG.RangeFloat(-MAX_DEVIATION, MAX_DEVIATION)) * originalGravity;
+        }
+
+        public override void OnStart()
+        {
+            tryApplyPatches();
+            base.OnStart();
+        }
+    }
+}
