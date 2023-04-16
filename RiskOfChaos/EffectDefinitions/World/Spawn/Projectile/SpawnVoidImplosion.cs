bb93fc8d6e256a303a369638e45078ca61a92e63
a22petoh <petter.ohlen@gmail.com>
2023-04-16 18:01:51 +0200

    Spawn Void Implosion: Use GenericSpawnEffect

--- RiskOfChaos/EffectDefinitions/World/Spawn/Projectile/SpawnVoidImplosion.cs
+++ RiskOfChaos/EffectDefinitions/World/Spawn/Projectile/SpawnVoidImplosion.cs
@@ -11,69 +11,34 @@ using UnityEngine.AddressableAssets;
 namespace RiskOfChaos.EffectDefinitions.World.Spawn.Projectile
 {
     [ChaosEffect("spawn_void_implosion")]
-    public sealed class SpawnVoidImplosion : BaseEffect
+    public sealed class SpawnVoidImplosion : GenericSpawnEffect<GameObject>
     {
-        readonly struct ImplosionProjectileInfo
-        {
-            public readonly GameObject ProjectilePrefab;
-            public readonly float Weight;
-
-            public readonly bool IsAvailable => ProjectilePrefab && ExpansionUtils.IsObjectExpansionAvailable(ProjectilePrefab);
-
-            public ImplosionProjectileInfo(GameObject prefab, float weight)
-            {
-                ProjectilePrefab = prefab;
-                Weight = weight;
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public static ImplosionProjectileInfo LoadFromAddressablePath(string path, float weight)
-            {
-                return new ImplosionProjectileInfo(Addressables.LoadAssetAsync<GameObject>(path).WaitForCompletion(), weight);
-            }
-        }
-        static ImplosionProjectileInfo[] _availableProjectiles;
+        static SpawnEntry[] _projectileEntries;
 
         [SystemInitializer]
         static void Init()
         {
-            _availableProjectiles = new ImplosionProjectileInfo[]
+            _projectileEntries = new SpawnEntry[]
             {
-                ImplosionProjectileInfo.LoadFromAddressablePath("RoR2/Base/Nullifier/NullifierDeathBombProjectile.prefab", 1f),
-                ImplosionProjectileInfo.LoadFromAddressablePath("RoR2/DLC1/VoidJailer/VoidJailerDeathBombProjectile.prefab", 0.4f),
-                ImplosionProjectileInfo.LoadFromAddressablePath("RoR2/DLC1/VoidMegaCrab/VoidMegaCrabDeathBombProjectile.prefab", 0.4f)
+                new SpawnEntry(Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Nullifier/NullifierDeathBombProjectile.prefab").WaitForCompletion(), 1f),
+                new SpawnEntry(Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidJailer/VoidJailerDeathBombProjectile.prefab").WaitForCompletion(), 0.4f),
+                new SpawnEntry(Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidMegaCrab/VoidMegaCrabDeathBombProjectile.prefab").WaitForCompletion(), 0.4f)
             };
         }
 
         [EffectCanActivate]
         static bool CanActivate()
         {
-            return _availableProjectiles != null && _availableProjectiles.Any(i => i.IsAvailable);
-        }
-
-        static WeightedSelection<GameObject> getProjectilePrefabSelection()
-        {
-            WeightedSelection<GameObject> weightedSelection = new WeightedSelection<GameObject>(_availableProjectiles.Length);
-            foreach (ImplosionProjectileInfo projectileInfo in _availableProjectiles)
-            {
-                if (projectileInfo.IsAvailable)
-                {
-                    weightedSelection.AddChoice(projectileInfo.ProjectilePrefab, projectileInfo.Weight);
-                }
-            }
-
-            return weightedSelection;
+            return areAnyAvailable(_projectileEntries);
         }
 
         public override void OnStart()
         {
-            WeightedSelection<GameObject> projectilePrefabSelection = getProjectilePrefabSelection();
-
             foreach (CharacterBody playerBody in PlayerUtils.GetAllPlayerBodies(true))
             {
                 ProjectileManager.instance.FireProjectile(new FireProjectileInfo
                 {
-                    projectilePrefab = projectilePrefabSelection.Evaluate(RNG.nextNormalizedFloat),
+                    projectilePrefab = getItemToSpawn(_projectileEntries, RNG),
                     position = playerBody.corePosition
                 });
             }
