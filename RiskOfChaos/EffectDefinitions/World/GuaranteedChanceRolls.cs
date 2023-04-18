73d7a1db5381277f9fc67ce152d1305e026c1534
a22petoh <petter.ohlen@gmail.com>
2023-04-18 17:11:58 +0200

    Added Effect: "Guaranteed Chance Effects"

---
+++ RiskOfChaos/EffectDefinitions/World/GuaranteedChanceRolls.cs
@@ -0,0 +1,41 @@
+﻿using RiskOfChaos.EffectHandling;
+using RiskOfChaos.EffectHandling.Controllers;
+using RiskOfChaos.EffectHandling.EffectClassAttributes;
+using RiskOfChaos.EffectHandling.EffectClassAttributes.Data;
+
+namespace RiskOfChaos.EffectDefinitions.World
+{
+    [ChaosEffect("guaranteed_chance_rolls", DefaultSelectionWeight = 0.9f, EffectStageActivationCountHardCap = 1, IsNetworked = true)]
+    public sealed class GuaranteedChanceRolls : TimedEffect
+    {
+        [InitEffectInfo]
+        static readonly ChaosEffectInfo _effectInfo;
+
+        static bool _hasAppliedPatches;
+        static void tryApplyPatches()
+        {
+            if (_hasAppliedPatches)
+                return;
+
+            On.RoR2.Util.CheckRoll_float_float_CharacterMaster += (orig, percentChance, luck, effectOriginMaster) =>
+            {
+                return orig(percentChance, luck, effectOriginMaster) || (percentChance > 0f &&
+                                                                         TimedChaosEffectHandler.Instance &&
+                                                                         TimedChaosEffectHandler.Instance.IsTimedEffectActive(_effectInfo));
+            };
+
+            _hasAppliedPatches = true;
+        }
+
+        public override TimedEffectType TimedType => TimedEffectType.UntilStageEnd;
+
+        public override void OnStart()
+        {
+            tryApplyPatches();
+        }
+
+        public override void OnEnd()
+        {
+        }
+    }
+}
