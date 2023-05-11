43c24ac37dd95e43f9c732898626d80a26d78aaf
a22petoh <petter.ohlen@gmail.com>
2023-05-11 15:34:17 +0200

    Added Effect: Invert Knockback

---
+++ RiskOfChaos/EffectDefinitions/World/ReverseKnockback.cs
@@ -0,0 +1,39 @@
+﻿using RiskOfChaos.EffectHandling;
+using RiskOfChaos.EffectHandling.EffectClassAttributes;
+using RiskOfChaos.EffectHandling.EffectClassAttributes.Methods;
+using RiskOfChaos.ModifierController.Knockback;
+using System;
+
+namespace RiskOfChaos.EffectDefinitions.World
+{
+    [ChaosEffect("reverse_knockback", EffectStageActivationCountHardCap = 1)]
+    [ChaosTimedEffect(TimedEffectType.UntilStageEnd)]
+    public sealed class ReverseKnockback : TimedEffect, IKnockbackModificationProvider
+    {
+        [EffectCanActivate]
+        static bool CanActivate()
+        {
+            return KnockbackModificationManager.Instance;
+        }
+
+        public override void OnStart()
+        {
+            KnockbackModificationManager.Instance.RegisterModificationProvider(this);
+        }
+
+        public event Action OnValueDirty;
+
+        public void ModifyValue(ref float value)
+        {
+            value *= -1f;
+        }
+
+        public override void OnEnd()
+        {
+            if (KnockbackModificationManager.Instance)
+            {
+                KnockbackModificationManager.Instance.UnregisterModificationProvider(this);
+            }
+        }
+    }
+}
