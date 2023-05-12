cb8f4e4331460dd787fe210bd2f6682c0331de50
a22petoh <petter.ohlen@gmail.com>
2023-05-12 19:28:36 +0200

    Ignore Tougher Times for guaranteed chance effects

--- RiskOfChaos/EffectDefinitions/World/GuaranteedChanceRolls.cs
+++ RiskOfChaos/EffectDefinitions/World/GuaranteedChanceRolls.cs
@@ -1,7 +1,11 @@
-﻿using RiskOfChaos.EffectHandling;
+﻿using HarmonyLib;
+using MonoMod.Cil;
+using RiskOfChaos.EffectHandling;
 using RiskOfChaos.EffectHandling.Controllers;
 using RiskOfChaos.EffectHandling.EffectClassAttributes;
 using RiskOfChaos.EffectHandling.EffectClassAttributes.Data;
+using RoR2;
+using System;
 
 namespace RiskOfChaos.EffectDefinitions.World
 {
@@ -14,6 +18,7 @@ namespace RiskOfChaos.EffectDefinitions.World
         static readonly ChaosEffectInfo _effectInfo;
 
         static bool _hasAppliedPatches;
+        static int _tempDisablePatchCount;
         static void tryApplyPatches()
         {
             if (_hasAppliedPatches)
@@ -22,8 +27,29 @@ namespace RiskOfChaos.EffectDefinitions.World
             On.RoR2.Util.CheckRoll_float_float_CharacterMaster += (orig, percentChance, luck, effectOriginMaster) =>
             {
                 return orig(percentChance, luck, effectOriginMaster) || (percentChance > 0f &&
+                                                                         _tempDisablePatchCount <= 0 &&
                                                                          TimedChaosEffectHandler.Instance &&
                                                                          TimedChaosEffectHandler.Instance.IsTimedEffectActive(_effectInfo));
+            };
+
+            IL.RoR2.HealthComponent.TakeDamage += il =>
+            {
+                ILCursor c = new ILCursor(il);
+
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public
+                const string ITEM_COUNTS_BEAR_FIELD_NAME = nameof(HealthComponent.ItemCounts.bear);
+#pragma warning restore Publicizer001 // Accessing a member that was not originally public
+
+                if (c.TryFindNext(out ILCursor[] foundCursors,
+                                  x => x.MatchLdfld<HealthComponent.ItemCounts>(ITEM_COUNTS_BEAR_FIELD_NAME),
+                                  x => x.MatchCallOrCallvirt(SymbolExtensions.GetMethodInfo(() => Util.CheckRoll(default, default, default)))))
+                {
+                    ILCursor cursor = foundCursors[1];
+
+                    cursor.EmitDelegate<Action>(() => _tempDisablePatchCount++);
+                    cursor.Index++;
+                    cursor.EmitDelegate<Action>(() => _tempDisablePatchCount--);
+                }
             };
 
             _hasAppliedPatches = true;
@@ -32,10 +58,12 @@ namespace RiskOfChaos.EffectDefinitions.World
         public override void OnStart()
         {
             tryApplyPatches();
+            _tempDisablePatchCount = 0;
         }
 
         public override void OnEnd()
         {
+            _tempDisablePatchCount = 0;
         }
     }
 }
