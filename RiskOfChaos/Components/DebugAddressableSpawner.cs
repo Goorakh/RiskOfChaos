e98e21a839a238f8fbaa3eec4822d11e322fb966
a22petoh <petter.ohlen@gmail.com>
2023-05-15 07:46:30 +0200

    Add DebugAddressableSpawner

---
+++ RiskOfChaos/Components/DebugAddressableSpawner.cs
@@ -0,0 +1,26 @@
+﻿using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace RiskOfChaos.Components
+{
+#if DEBUG
+    public class DebugAddressableSpawner : MonoBehaviour
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            GameObject debugAddressableSpawner = new GameObject("DebugAddressableSpawner");
+            DontDestroyOnLoad(debugAddressableSpawner);
+
+            debugAddressableSpawner.AddComponent<DebugAddressableSpawner>();
+        }
+
+        public GameObject SpawnAddressablePrefab(string path)
+        {
+            GameObject prefab = Addressables.LoadAssetAsync<GameObject>(path).WaitForCompletion();
+            return Instantiate(prefab);
+        }
+    }
+#endif
+}
