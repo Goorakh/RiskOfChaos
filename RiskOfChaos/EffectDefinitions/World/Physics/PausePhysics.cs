c425a62488204b34413bc6f9b00d754d00fdcf9d
a22petoh <petter.ohlen@gmail.com>
2023-05-25 23:46:47 +0200

    Pause Physics duration: 90s -> 40s

--- RiskOfChaos/EffectDefinitions/World/Physics/PausePhysics.cs
+++ RiskOfChaos/EffectDefinitions/World/Physics/PausePhysics.cs
@@ -3,7 +3,7 @@
 namespace RiskOfChaos.EffectDefinitions.World.Physics
 {
     [ChaosEffect("pause_physics")]
-    [ChaosTimedEffect(90f, AllowDuplicates = false)]
+    [ChaosTimedEffect(40f, AllowDuplicates = false)]
     public sealed class PausePhysics : SimplePhysicsSpeedMultiplierEffect
     {
         protected override float multiplier => 0f;
