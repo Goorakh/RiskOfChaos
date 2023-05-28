40e126c68304e979b1bb75ce4a730821c7f7f1c0
a22petoh <petter.ohlen@gmail.com>
2023-05-29 00:20:14 +0200

    Teleport on blastattack miss

--- RiskOfChaos/EffectDefinitions/Character/TeleportingProjectiles.cs
+++ RiskOfChaos/EffectDefinitions/Character/TeleportingProjectiles.cs
@@ -11,7 +11,7 @@ using UnityEngine.Networking;
 
 namespace RiskOfChaos.EffectDefinitions.Character
 {
-    [ChaosEffect("teleporting_projectiles", DefaultSelectionWeight = 0.7f)]
+    [ChaosEffect("teleporting_projectiles", DefaultSelectionWeight = 0.7f, IsNetworked = true)]
     [ChaosTimedEffect(30f, AllowDuplicates = false)]
     [EffectConfigBackwardsCompatibility("Effect: Teleporting Attacks (Lasts until next effect)")]
     public sealed class TeleportingProjectiles : TimedEffect
@@ -46,25 +46,57 @@ namespace RiskOfChaos.EffectDefinitions.Character
                 if (!NetworkServer.active)
                     return;
 
-                if (!TimedChaosEffectHandler.Instance || !TimedChaosEffectHandler.Instance.IsTimedEffectActive(_effectInfo))
-                    return;
+                tryTeleportAttacker(damageInfo.attacker, damageInfo.position);
+            };
 
-                GameObject attackerObj = damageInfo.attacker;
-                if (!attackerObj)
-                    return;
+            On.RoR2.BlastAttack.Fire += (orig, self) =>
+            {
+                BlastAttack.Result result = orig(self);
 
-                CharacterBody attackerBody = attackerObj.GetComponent<CharacterBody>();
-                if (!attackerBody)
-                    return;
+                if (result.hitCount == 0)
+                {
+                    tryTeleportAttacker(self.attacker, self.position);
+                }
 
-                CharacterMaster master = attackerBody.master;
-                if (!master || Array.IndexOf(_teleportBlacklist, master.masterIndex) != -1)
-                    return;
-
-                TeleportUtils.TeleportBody(attackerBody, damageInfo.position);
+                return result;
             };
 
             _appliedPatches = true;
+        }
+
+        static void tryTeleportAttacker(GameObject attackerObj, Vector3 teleportPosition)
+        {
+            if (!attackerObj)
+                return;
+
+            if (!TimedChaosEffectHandler.Instance || !TimedChaosEffectHandler.Instance.IsTimedEffectActive(_effectInfo))
+                return;
+
+            CharacterBody attackerBody = attackerObj.GetComponent<CharacterBody>();
+            if (!attackerBody)
+                return;
+
+            CharacterMaster master = attackerBody.master;
+            if (!master || Array.IndexOf(_teleportBlacklist, master.masterIndex) != -1)
+                return;
+
+            /*
+            const float MIN_TELEPORT_DISTANCE = 0.1f;
+            float sqrTeleportDistance = (attackerBody.footPosition - teleportPosition).sqrMagnitude;
+            if (sqrTeleportDistance < MIN_TELEPORT_DISTANCE * MIN_TELEPORT_DISTANCE)
+            {
+#if DEBUG
+                Log.Debug($"Not teleporting {Util.GetBestBodyName(attackerObj)}: Teleport distance too short to consider ({Mathf.Sqrt(sqrTeleportDistance)} units)");
+#endif
+                return;
+            }
+
+#if DEBUG
+            Log.Debug($"Teleporting {Util.GetBestBodyName(attackerObj)} {Mathf.Sqrt(sqrTeleportDistance)} units");
+#endif
+            */
+
+            TeleportUtils.TeleportBody(attackerBody, teleportPosition);
         }
 
         public override void OnStart()
