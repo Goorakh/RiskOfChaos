7fe625c8e0eb7838fbaea1fd34772c87489fe9dc
a22petoh <petter.ohlen@gmail.com>
2023-09-19 23:29:08 +0200

    Add disabled check to RunEffectsTimerWhileRunTimerPaused config

--- RiskOfChaos/Config/General.cs
+++ RiskOfChaos/Config/General.cs
@@ -15,13 +15,15 @@ namespace RiskOfChaos
                                    .OptionConfig(new CheckBoxConfig())
                                    .Build();
 
+            static bool effectDispatchingDisabled() => DisableEffectDispatching.Value;
+
             const float TIME_BETWEEN_EFFECTS_MIN_VALUE = 5f;
             public static readonly ConfigHolder<float> TimeBetweenEffects =
                 ConfigFactory<float>.CreateConfig("Effect Timer", 60f)
                                     .Description("How often new effects should happen")
                                     .OptionConfig(new StepSliderConfig
                                     {
-                                        checkIfDisabled = () => DisableEffectDispatching.Value,
+                                        checkIfDisabled = effectDispatchingDisabled,
                                         formatString = "{0:F0}s",
                                         increment = 5f,
                                         min = TIME_BETWEEN_EFFECTS_MIN_VALUE,
@@ -33,7 +35,10 @@ namespace RiskOfChaos
             public static readonly ConfigHolder<bool> RunEffectsTimerWhileRunTimerPaused =
                 ConfigFactory<bool>.CreateConfig("Dispatch Effects While Timer Paused", true)
                                    .Description("If the mod should activate effects while the run timer is paused (in Bazaar, Gilded Coast, etc.)")
-                                   .OptionConfig(new CheckBoxConfig())
+                                   .OptionConfig(new CheckBoxConfig
+                                   {
+                                       checkIfDisabled = effectDispatchingDisabled
+                                   })
                                    .Build();
 
             public static readonly ConfigHolder<Color> ActiveEffectsTextColor =
