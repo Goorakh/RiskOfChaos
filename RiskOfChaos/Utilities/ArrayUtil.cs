dc2dbed2b0ce05ac459d8456d3b8a7ebe8d42234
Gorakh <petter.ohlen@gmail.com>
2023-12-23 17:53:54 +0100

    Improve ArrayUtil.AppendRange logic, avoid unnecessary assignment, also support null values

--- RiskOfChaos/Utilities/ArrayUtil.cs
+++ RiskOfChaos/Utilities/ArrayUtil.cs
@@ -7,20 +7,14 @@ namespace RiskOfChaos.Utilities
     {
         public static void AppendRange<T>(ref T[] array, T[] appended)
         {
-            if (array is null)
-                throw new ArgumentNullException(nameof(array));
+            if (appended is null || appended.Length == 0)
+                return;
 
-            if (appended is null)
-                throw new ArgumentNullException(nameof(appended));
-
-            if (array.Length == 0)
+            if (array is null || array.Length == 0)
             {
                 array = appended;
                 return;
             }
-
-            if (appended.Length == 0)
-                return;
 
             int oldLength = array.Length;
             Array.Resize(ref array, oldLength + appended.Length);
