2522eda9c4c2dde6e5605791717df3a17e6beb87
Gorakh <petter.ohlen@gmail.com>
2024-03-26 18:33:34 +0100

    Refactor CustomPlayerDeathMessageTokenPatch

--- RiskOfChaos/Patches/CustomPlayerDeathMessageTokenPatch.cs
+++ RiskOfChaos/Patches/CustomPlayerDeathMessageTokenPatch.cs
@@ -1,7 +1,6 @@
 ﻿using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using RoR2;
-using System;
 
 namespace RiskOfChaos.Patches
 {
@@ -18,46 +17,45 @@ namespace RiskOfChaos.Patches
 
         static void GlobalEventManager_OnPlayerCharacterDeath(ILContext il)
         {
-            Instruction[] instructions = il.Body.Instructions.ToArray();
-
-            int localSearchStartIndex = Array.FindIndex(instructions, i => i.MatchLdstr("PLAYER_DEATH_QUOTE_VOIDDEATH"));
-            if (localSearchStartIndex == -1)
-            {
-                Log.Error("Failed to find death message token location");
-                return;
-            }
-
-            int deathTokenLocalIndex = -1;
-
-            for (int i = localSearchStartIndex + 1; i < instructions.Length; i++)
-            {
-                if (instructions[i].MatchStloc(out int localIndex))
-                {
-                    deathTokenLocalIndex = localIndex;
-                    break;
-                }
-            }
-
-            if (deathTokenLocalIndex == -1)
-            {
-                Log.Error("Failed to find death message token local index");
-                return;
-            }
-
-#if DEBUG
-            Log.Debug($"Death message token local index: {deathTokenLocalIndex}");
-#endif
-
             ILCursor c = new ILCursor(il);
 
-            while (c.TryGotoNext(MoveType.After, x => x.MatchLdloc(deathTokenLocalIndex)))
+            if (c.TryGotoNext(x => x.MatchLdstr("PLAYER_DEATH_QUOTE_VOIDDEATH")))
             {
-                c.Emit(OpCodes.Ldarg_1); // DamageReport damageReport
-                c.EmitDelegate((string messageToken, DamageReport damageReport) =>
+                int deathMessageTokenLocalIndex = -1;
+                if (c.TryGotoNext(x => x.MatchStloc(out deathMessageTokenLocalIndex)))
                 {
-                    OverridePlayerDeathMessageToken?.Invoke(damageReport, ref messageToken);
-                    return messageToken;
-                });
+#if DEBUG
+                    Log.Debug($"Death message token local index: {deathMessageTokenLocalIndex}");
+#endif
+
+                    ILLabel messageTokenDecidedLabel = null;
+                    if (c.TryGotoNext(x => x.MatchBr(out messageTokenDecidedLabel)))
+                    {
+                        c.GotoLabel(messageTokenDecidedLabel);
+                        c.Emit(OpCodes.Ldloca, deathMessageTokenLocalIndex);
+                        c.Emit(OpCodes.Ldarg_1); // DamageReport damageReport
+                        c.EmitDelegate((ref string messageToken, DamageReport damageReport) =>
+                        {
+#if DEBUG
+                            Log.Debug($"Overriding death message token: {messageToken}");
+#endif
+
+                            OverridePlayerDeathMessageToken?.Invoke(damageReport, ref messageToken);
+                        });
+                    }
+                    else
+                    {
+                        Log.Error("Failed to find patch location");
+                    }
+                }
+                else
+                {
+                    Log.Error("Failed to find death message token local index");
+                }
+            }
+            else
+            {
+                Log.Error("Failed to find death message token location");
             }
         }
     }
