ac243260a49fd1e84bd178590db5ddf3e377e482
Gorakh <petter.ohlen@gmail.com>
2024-09-16 21:36:49 +0200

    Update loggers

--- RiskOfChaos/Log.cs
+++ RiskOfChaos/Log.cs
@@ -1,50 +1,131 @@
 ﻿using BepInEx.Logging;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace RiskOfChaos
 {
     internal static class Log
     {
-        internal static ManualLogSource _logSource;
+        static readonly object _stringBuilderLock = new object();
+        static readonly StringBuilder _sharedStringBuilder = new StringBuilder(256);
+
+        static readonly int _cachedCallerPathPrefixLength;
+
+        static ManualLogSource _logSource;
+
+        static Log()
+        {
+            _cachedCallerPathPrefixLength = getCallerPathPrefixLength();
+
+            static int getCallerPathPrefixLength([CallerFilePath] string callerPath = null)
+            {
+                const string MOD_NAME = nameof(RiskOfChaos) + @"\";
+
+                int modNameLastPathIndex = callerPath.LastIndexOf(MOD_NAME);
+                if (modNameLastPathIndex >= 0)
+                {
+                    return modNameLastPathIndex + MOD_NAME.Length;
+                }
+                else
+                {
+                    UnityEngine.Debug.LogError($"[{Main.PluginName}] Logger failed to determine caller path prefix length");
+                    return 0;
+                }
+            }
+        }
 
         internal static void Init(ManualLogSource logSource)
         {
             _logSource = logSource;
         }
 
-        static string getLogPrefix(string callerPath, string callerMemberName, int callerLineNumber)
+        static StringBuilder AppendCallerPrefix(this StringBuilder stringBuilder, string callerPath, string callerMemberName, int callerLineNumber)
         {
-            const string MOD_NAME = "RiskOfChaos";
+            return stringBuilder.Append(callerPath, _cachedCallerPathPrefixLength, callerPath.Length - _cachedCallerPathPrefixLength)
+                                .Append(':').Append(callerLineNumber)
+                                .Append(" (").Append(callerMemberName).Append("):");
+        }
 
-            int modNameLastPathIndex = callerPath.LastIndexOf(MOD_NAME);
-            if (modNameLastPathIndex >= 0)
+        static StringBuilder buildCallerLogString(string callerPath, string callerMemberName, int callerLineNumber, object data)
+        {
+            lock (_stringBuilderLock)
             {
-                callerPath = callerPath.Substring(modNameLastPathIndex + MOD_NAME.Length + 1);
+                return _sharedStringBuilder.Clear()
+                                           .AppendCallerPrefix(callerPath, callerMemberName, callerLineNumber)
+                                           .Append(' ')
+                                           .Append(data);
             }
-
-            return $"{callerPath}:{callerLineNumber} ({callerMemberName}) ";
         }
 
 #if DEBUG
-        internal static void Debug(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogDebug(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data);
-        internal static void Debug_NoCallerPrefix(object data) => _logSource.LogDebug(data);
+        internal static void Debug(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            _logSource.LogDebug(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Debug_NoCallerPrefix(object data)
+        {
+            _logSource.LogDebug(data);
+        }
 #endif
 
-        internal static void Error(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogError(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data);
-        internal static void Error_NoCallerPrefix(object data) => _logSource.LogError(data);
+        internal static void Error(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            _logSource.LogError(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data));
+        }
 
-        internal static void Fatal(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogFatal(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data);
-        internal static void Fatal_NoCallerPrefix(object data) => _logSource.LogFatal(data);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Error_NoCallerPrefix(object data)
+        {
+            _logSource.LogError(data);
+        }
 
-        internal static void Info(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogInfo(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data);
-        internal static void Info_NoCallerPrefix(object data) => _logSource.LogInfo(data);
+        internal static void Fatal(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            _logSource.LogFatal(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data));
+        }
 
-        internal static void Message(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogMessage(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data);
-        internal static void Message_NoCallerPrefix(object data) => _logSource.LogMessage(data);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Fatal_NoCallerPrefix(object data)
+        {
+            _logSource.LogFatal(data);
+        }
 
-        internal static void Warning(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => _logSource.LogWarning(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data);
-        internal static void Warning_NoCallerPrefix(object data) => _logSource.LogWarning(data);
+        internal static void Info(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            _logSource.LogInfo(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data));
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Info_NoCallerPrefix(object data)
+        {
+            _logSource.LogInfo(data);
+        }
+
+        internal static void Message(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            _logSource.LogMessage(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Message_NoCallerPrefix(object data)
+        {
+            _logSource.LogMessage(data);
+        }
+
+        internal static void Warning(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            _logSource.LogWarning(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Warning_NoCallerPrefix(object data)
+        {
+            _logSource.LogWarning(data);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static bool ShouldLog(LogLevel logLevel)
         {
 #if !DEBUG
--- RiskOfTwitch/Log.cs
+++ RiskOfTwitch/Log.cs
@@ -1,5 +1,6 @@
 ﻿using RiskOfTwitch.Logging;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace RiskOfTwitch
 {
@@ -7,37 +8,116 @@ namespace RiskOfTwitch
     {
         public static ILogSource LogSource { get; set; } = new ConsoleLogSource();
 
-        static string getLogPrefix(string callerPath, string callerMemberName, int callerLineNumber)
+        static readonly object _stringBuilderLock = new object();
+        static readonly StringBuilder _sharedStringBuilder = new StringBuilder(256);
+
+        static readonly int _cachedCallerPathPrefixLength;
+
+        static Log()
         {
-            const string PATH_NAME = "RiskOfTwitch";
+            _cachedCallerPathPrefixLength = getCallerPathPrefixLength();
 
-            int nameLastPathIndex = callerPath.LastIndexOf(PATH_NAME);
-            if (nameLastPathIndex >= 0)
+            static int getCallerPathPrefixLength([CallerFilePath] string callerPath = null)
             {
-                callerPath = callerPath.Substring(nameLastPathIndex);
-            }
+                const string PATH_NAME = "RiskOfTwitch";
 
-            return $"{callerPath}:{callerLineNumber} ({callerMemberName}) ";
+                int pathNameLastPathIndex = callerPath.LastIndexOf(PATH_NAME);
+                if (pathNameLastPathIndex >= 0)
+                {
+                    return pathNameLastPathIndex;
+                }
+                else
+                {
+                    Error_NoCallerPrefix($"[{PATH_NAME}] Logger failed to determine caller path prefix length");
+                    return 0;
+                }
+            }
+        }
+
+        static StringBuilder AppendCallerPrefix(this StringBuilder stringBuilder, string callerPath, string callerMemberName, int callerLineNumber)
+        {
+            return stringBuilder.Append(callerPath, _cachedCallerPathPrefixLength, callerPath.Length - _cachedCallerPathPrefixLength)
+                                .Append(':').Append(callerLineNumber)
+                                .Append(" (").Append(callerMemberName).Append("):");
+        }
+
+        static StringBuilder buildCallerLogString(string callerPath, string callerMemberName, int callerLineNumber, object data)
+        {
+            lock (_stringBuilderLock)
+            {
+                return _sharedStringBuilder.Clear()
+                                           .AppendCallerPrefix(callerPath, callerMemberName, callerLineNumber)
+                                           .Append(' ')
+                                           .Append(data);
+            }
         }
 
 #if DEBUG
-        internal static void Debug(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => LogSource.Log(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data, LogType.Debug);
-        internal static void Debug_NoCallerPrefix(object data) => LogSource.Log(data, LogType.Debug);
+        internal static void Debug(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            LogSource.Log(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data), LogType.Debug);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Debug_NoCallerPrefix(object data)
+        {
+            LogSource.Log(data, LogType.Debug);
+        }
 #endif
 
-        internal static void Error(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => LogSource.Log(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data, LogType.Error);
-        internal static void Error_NoCallerPrefix(object data) => LogSource.Log(data, LogType.Error);
+        internal static void Error(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            LogSource.Log(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data), LogType.Error);
+        }
 
-        internal static void Fatal(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => LogSource.Log(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data, LogType.Fatal);
-        internal static void Fatal_NoCallerPrefix(object data) => LogSource.Log(data, LogType.Fatal);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Error_NoCallerPrefix(object data)
+        {
+            LogSource.Log(data, LogType.Error);
+        }
 
-        internal static void Info(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => LogSource.Log(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data, LogType.Info);
-        internal static void Info_NoCallerPrefix(object data) => LogSource.Log(data, LogType.Info);
+        internal static void Fatal(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            LogSource.Log(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data), LogType.Fatal);
+        }
 
-        internal static void Message(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => LogSource.Log(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data, LogType.Message);
-        internal static void Message_NoCallerPrefix(object data) => LogSource.Log(data, LogType.Message);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Fatal_NoCallerPrefix(object data)
+        {
+            LogSource.Log(data, LogType.Fatal);
+        }
 
-        internal static void Warning(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1) => LogSource.Log(getLogPrefix(callerPath, callerMemberName, callerLineNumber) + data, LogType.Warning);
-        internal static void Warning_NoCallerPrefix(object data) => LogSource.Log(data, LogType.Warning);
+        internal static void Info(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            LogSource.Log(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data), LogType.Info);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Info_NoCallerPrefix(object data)
+        {
+            LogSource.Log(data, LogType.Info);
+        }
+
+        internal static void Message(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            LogSource.Log(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data), LogType.Message);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Message_NoCallerPrefix(object data)
+        {
+            LogSource.Log(data, LogType.Message);
+        }
+
+        internal static void Warning(object data, [CallerFilePath] string callerPath = "", [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            LogSource.Log(buildCallerLogString(callerPath, callerMemberName, callerLineNumber, data), LogType.Warning);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Warning_NoCallerPrefix(object data)
+        {
+            LogSource.Log(data, LogType.Warning);
+        }
     }
 }
