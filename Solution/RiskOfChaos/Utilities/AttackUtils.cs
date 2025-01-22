0a00c930faea705d016a4202f18bd4f16836872b
Gorakh <petter.ohlen@gmail.com>
2025-01-22 20:10:32 +0100

    Nullcheck attack instances before cloning

--- Solution/RiskOfChaos/Utilities/AttackUtils.cs
+++ Solution/RiskOfChaos/Utilities/AttackUtils.cs
@@ -8,16 +8,25 @@ namespace RiskOfChaos.Utilities
     {
         public static BulletAttack Clone(BulletAttack src)
         {
+            if (src == null)
+                return null;
+
             return src.ShallowCopy(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         }
 
         public static BlastAttack Clone(BlastAttack src)
         {
+            if (src == null)
+                return null;
+
             return src.ShallowCopy();
         }
 
         public static OverlapAttack Clone(OverlapAttack src)
         {
+            if (src == null)
+                return null;
+
             return src.ShallowCopy(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         }
     }
